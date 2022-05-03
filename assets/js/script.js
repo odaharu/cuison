b8fef188928e5f4b6be3670dfb6e451e2c13a063
odaharu <odaharu.0119@gmail.com>
2022-05-04 00:17:47 +0900

    add first

---
+++ assets/js/script.js
@@ -0,0 +1,36 @@
+/*↓ハンバーガー */
+
+$(function () {
+  $('.js-btn').on('click', function () {        // js-btnクラスをクリックすると、
+    $('.menu , .btn-line').toggleClass('open'); // メニューとバーガーの線にopenクラスをつけ外しする
+  })
+});
+
+
+
+/*↓上に戻るボタン*/
+
+jQuery(function() {
+  var pagetop = $('#page_top');   
+  pagetop.hide();
+  $(window).scroll(function () {
+      if ($(this).scrollTop() > 850) {  //100pxスクロールしたら表示
+          pagetop.fadeIn();
+      } else {
+          pagetop.fadeOut();
+      }
+  });
+$('a[href^="#"]').click(function(){
+  var time = 500;
+  var href= $(this).attr("href");
+  var target = $(href == "#" ? 'html' : href);
+  var distance = target.offset().top;
+  $("html, body").animate({scrollTop:distance}, time, "swing");
+  return false;
+});
+});
+
+
+
+
+
